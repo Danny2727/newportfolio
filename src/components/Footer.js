ae2a43c953aa033f3ed66eb44170570d52a65ff6
Meech <meech@dannys-mbp.home>
2023-06-14 20:48:53 -0400

    made chages to deploy portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer () {
     return (
         <footer >
           <a class="links" href='https://github.com/Danny2727'> <i class="fa-brands fa-github fa-spin fa-spin-reverse fa-2xl"></i></a>
-           <a class ="links" href='https://www.linkedin.com/feed/'><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+           <a class ="links" href='www.linkedin.com/in/daniel-edouard'><i class="fa-brands fa-linkedin fa-2xl"></i></a>
         </footer>
     )
 }
